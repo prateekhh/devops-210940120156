86e9beacd9b25c03c4acda41927f869a86b96770
prateekhh <cu.16bcs1199@gmail.com>
2022-01-30 17:20:22 +0530

    Initial Commit

---
+++ natural.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Natural{
+  public static void main(String args[]){
+   int i=1,n =10 , sum ;
+  while (i <= n) {
+   System.out.println(i) ;
+   i++; 
+} 
+  sum = (n*(n+1))/2 ;
+  System.out.println("Sum : " + sum);
+}
+}
